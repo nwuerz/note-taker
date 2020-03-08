98d928d459d5639ab551186f23cf0d832a8378ca
nick wuerz <nawuerz@gmail.com>
2020-03-08 16:36:15 -0500

    added routes for index and notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,21 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function() {
-    console.log(`App listening on PORT ${PORT}`);
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+//create arrays to store note data?
+
+//routes
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
