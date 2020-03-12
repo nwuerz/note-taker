94ac5c0b6062a8dae2bb119767b31bf9f1380929
nick wuerz <nawuerz@gmail.com>
2020-03-11 22:04:42 -0500

    fixed routes, added middleware and post function

--- Develop/server.js
+++ Develop/server.js
@@ -1,21 +1,56 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// (are these neccessary?)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join('public')))
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 
-//create arrays to store note data?
-
 //routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+//(working?)
+
+
+//api routes (not working)
+
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    
+    })
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.redirect('/')
 });
 
+let id = 1;
 
+app.post("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        const currentNotes = JSON.parse(data);
+        req.body.id = id;
+        id++;
+        console.log(req.body, id)
+        currentNotes.push(req.body);
+        const dataToSave = JSON.stringify(currentNotes);
+        fs.writeFile("./db/db.json", dataToSave, (err) => {
+            if (err) throw err;
+            res.sendStatus(200);
+        });
+    })
+});
